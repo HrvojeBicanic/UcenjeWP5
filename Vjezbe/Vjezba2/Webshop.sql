a915462073e9e01478ed611f5b23913b905d68af
Hrvoje Bičanić <73309097+HrvojeBicanic@users.noreply.github.com>
2024-06-18 17:27:33 +0200

    insert i update kupaca

--- Vjezbe/Vjezba2/Webshop.sql
+++ Vjezbe/Vjezba2/Webshop.sql
@@ -43,3 +43,10 @@ insert into proizvodi (naziv, datum_nabave, cijena, aktivan) values
 	('Jogurt','03-04-2023 12:11', 2.10, 0),
 	('Batak','02-04-2023 09:23', 50.25, 1)
 update proizvodi set naziv='Kisela', aktivan=0 where naziv='Voda';
+
+select * from kupci;
+insert into kupci (ime, prezime, mjesto) values
+	('Damir','Mirda','Švajcarnica'),
+	('Vojko','Vucina','Svinjarevci'),
+	('Bizzo','Bodega','Privlaka')
+update kupci set prezime='Vrućina' where prezime='Vucina';
