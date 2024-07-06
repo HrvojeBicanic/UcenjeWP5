a5667a7213eb323ff4e2bc7bf7c967bf6db6bd06
Hrvoje Bičanić <73309097+HrvojeBicanic@users.noreply.github.com>
2024-07-06 15:28:32 +0200

    E01Z4

--- CSHARP/Ucenje/UcenjeCS/E01Z4.cs
+++ CSHARP/Ucenje/UcenjeCS/E01Z4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,8 @@ namespace UcenjeCS
         //Koristeći ASCII art (https://www.asciiart.eu/) ispišite vama omiljenu grafiku.
         public static void Izvedi() 
         {
-            Console.WriteLine("|\\---/|\r\n| o_o |\r\n \\_^_/");
 
+            Console.WriteLine("   _=====_                               _=====_\r\n     / _____ \\                             / _____ \\\r\n   +.-'_____'-.---------------------------.-'_____'-.+\r\n  /   |     |  '.        S O N Y        .'  |  _  |   \\\r\n / ___| /|\\ |___ \\                     / ___| /_\\ |___ \\\r\n/ |      |      | ;  __           _   ; | _         _ | ;\r\n| | <---   ---> | | |__|         |_:> | ||_|       (_)| |\r\n| |___   |   ___| ;SELECT       START ; |___       ___| ;\r\n|\\    | \\|/ |    /  _     ___      _   \\    | (X) |    /|\r\n| \\   |_____|  .','\" \"', |___|  ,'\" \"', '.  |_____|  .' |\r\n|  '-.______.-' /       \\ANALOG/       \\  '-._____.-'   |\r\n|               |       |------|       |                |\r\n|              /\\       /      \\       /\\               |\r\n|             /  '.___.'        '.___.'  \\              |\r\n|            /                            \\             |\r\n \\          /                              \\           /\r\n  \\________/                                \\_________/");
         }
     }
 }
