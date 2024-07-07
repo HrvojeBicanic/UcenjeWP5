4f3762e2e2c644418d94df7e1848b7a670aeb125
Hrvoje Bičanić <73309097+HrvojeBicanic@users.noreply.github.com>
2024-07-07 10:27:33 +0200

    E01Z3 - program sada od korisnika zahtjeva da unese broj, ako broj nije unesen program se ne zavrsava

--- CSHARP/Ucenje/UcenjeCS/E01Z3.cs
+++ CSHARP/Ucenje/UcenjeCS/E01Z3.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +15,39 @@ namespace UcenjeCS
         public static void Izvedi() 
         {
             Console.WriteLine("Unesi ime grada: ");
-            var grad = Console.ReadLine();
+            string grad = Console.ReadLine();
+
             Console.WriteLine("Unesi broj stanovnika :");
-            int brojStanovnika = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("U " + grad + " živi " + brojStanovnika + " ljudi.");
-            //Console.WriteLine("U {0} živi {1} ljudi", grad, brojStanovnika);
-            Console.WriteLine($"U {grad} zivi {brojStanovnika} ljudi");
+            /* Ovaj dio koda osigurava da korsinik upise broj stanovnika i grad,
+             * ali ako korisnik ne upise broj program se prekida i ne trazi od korisnika ponovno da upiše broj
+             * 
+            if (int.TryParse(Console.ReadLine(), out int brojStanovnika))
+            {
+                //Console.WriteLine("U " + grad + " živi " + brojStanovnika + " ljudi.");
+                //Console.WriteLine("U {0} živi {1} ljudi", grad, brojStanovnika); --> ispravno ali, donji nacin je također moguc 
+                Console.WriteLine($"U {grad} zivi {brojStanovnika} ljudi");
+            }
+            else
+            {
+                Console.WriteLine("Unos nije broj");
+                
+            }*/
 
+            while (true) 
+            {
+                if (int.TryParse(Console.ReadLine(), out int brojStanovnika))
+                {
+                    Console.WriteLine($"Grad {grad} ima {brojStanovnika}");
+                }
+                else 
+                {
+                    Console.WriteLine("Unos nije valjan!");
+                    Console.Write("Molim unesite broj stanovnika: ");
+                    continue;
+                }
+            
+            }
         }
     }
 }
