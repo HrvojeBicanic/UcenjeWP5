71540b3ffcc91512653525e6a949418c5ed8379e
Hrvoje Bičanić <73309097+HrvojeBicanic@users.noreply.github.com>
2024-09-12 20:00:23 +0200

    12.09.

---
+++ CSHARP/Ucenje/EdunovaAPP/Models/Smjer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Models
+{
+    public class Smjer : Entitet
+    {
+        public String? Naziv { get; set; }
+        [Column("brojsati")] // ovo nuzno u vasim zavrsnim radovima nije potrebno
+        public int? Tranjanje { get; set; }
+        public decimal? Cijena { get; set; }
+        public DateTime? IzvodiSeOd { get; set; }
+        public bool? Vaucer { get; set; }
+    }
+}
